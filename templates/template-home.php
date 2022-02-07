c1bda79402301d9aee45f63dbf14954d9bb1b4e0
WilliamCaouette <1841363@cgmatane.qc.ca>
2022-02-07 17:38:08 -0500

    ajout du template de la page d'accueil

---
+++ templates/template-home.php
@@ -0,0 +1,59 @@
+<?php 
+/*
+Template Name: Gabarit accueil
+*/
+    get_header();
+    
+    //$args = array('category_name'=>'Techno', 'post_type' => 'post');
+    //$query = new WP_Query($args);
+
+    $args = array('category__not_in' => 1);
+    $query = new WP_Query($args);
+
+
+    ?>
+    <div class="flex">
+            <section class="trois-quart">
+            <?php
+            
+                if($query->have_posts()){
+
+                    while ($query->have_posts())
+                    { 
+                        $query->the_post() 
+                    ?>
+                    <article class="flex">
+                        <div class="image-article quart">
+                        <?php 
+                            if(has_post_thumbnail()){
+                                the_post_thumbnail('small', ['class' => 'fluide'] );
+                            }
+                        ?>   
+                        </div>
+                        <div class="texte-article trois-quart">
+                            <h3><?=the_title();?></h3>
+                            <p><?=the_excerpt();?><a href="<?=the_permalink();?>">En savoir plus...</a></p>
+                            <p><?=the_tags();?></p>
+                        </div>
+                    </article>
+                    <?php
+                    }
+                }
+            ?>
+            </section>
+            <section class="quart sidebar">
+                <article>
+                    <h3>Sidebar</h3>
+                    <div class="widget-container">
+                        <!-- widget -->
+                    </div>
+                    <div class="widget-container">
+                        <!-- widget -->
+                    </div>
+                </article>
+            </section>
+        </div>
+    
+    <?php
+    get_footer();
+?>

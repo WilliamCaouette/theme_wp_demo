a30f941ca42cf7069c93b76df1d9d55dcc475945
WilliamCaouette <1841363@cgmatane.qc.ca>
2022-02-07 16:57:56 -0500

    ajout du template de page

---
+++ page.php
@@ -0,0 +1,19 @@
+<?php
+    get_header( );
+    if(have_posts()){
+        while (have_posts()){ 
+            the_post();
+        ?>
+            <article class="flex">
+                <div class="text-article">
+                        <h2><?=the_title( );?></h2>
+                        <p><?=the_content( );?></p>
+                </div>
+            </article>
+    <?php
+        }
+    }
+    ?>
+<?
+    get_footer();
+?>
